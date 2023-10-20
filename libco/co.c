3b5b248c4e4c79f075c34d9a5004d38f7705246c
tracer-nju <tracer@nju.edu.cn>
2023-10-20 11:20:06 -0400

    Linux localhost.localdomain 5.1.0-splitfs-default-config #6 SMP Fri Aug 4 06:11:57 EDT 2023 x86_64 x86_64 x86_64 GNU/Linux  11:20:06 up 2 days, 11:28,  1 user,  load average: 0.16, 0.23, 0.16

--- libco/co.c
+++ libco/co.c
@@ -167,6 +167,7 @@ void co_yield (void)
             printf("before %ld\n", prev_sp);
             stack_switch_call(next->stack + STACK_SIZE, next->func, (uintptr_t)next->arg);
             printf("after %ld\n", prev_sp);
+            sleep(10);
             assert(prev_sp != 0);
             set_stack_pointer(prev_sp);
             printf("co %s finished\n", next->name);
