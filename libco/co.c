4680fa0b6b11efc6789dc9ace345906701fb96c6
tracer-nju <tracer@nju.edu.cn>
2023-10-20 11:25:50 -0400

    Linux localhost.localdomain 5.1.0-splitfs-default-config #6 SMP Fri Aug 4 06:11:57 EDT 2023 x86_64 x86_64 x86_64 GNU/Linux  11:25:50 up 2 days, 11:34,  1 user,  load average: 0.18, 0.18, 0.15

--- libco/co.c
+++ libco/co.c
@@ -165,9 +165,9 @@ void co_yield (void)
             next->status = CO_RUNNING;
             current = next;
             stack_switch_call(next->stack + STACK_SIZE, next->func, (uintptr_t)next->arg);
-            printf("co '%s' finished\n", current->name);
             unmanage_co(current);
             current->status = CO_DEAD;
+            printf("co '%s' finished\n", current->name);
         } else {
             current = next;
             longjmp(next->context, SWITCH_IN);
