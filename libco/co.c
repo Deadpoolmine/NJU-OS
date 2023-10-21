5fba01c67efbc84089e5311c9c495e505d325b89
tracer-nju <tracer@nju.edu.cn>
2023-10-20 22:56:34 -0400

    Linux localhost.localdomain 5.1.0-splitfs-default-config #6 SMP Fri Aug 4 06:11:57 EDT 2023 x86_64 x86_64 x86_64 GNU/Linux  22:56:34 up 2 days, 23:04,  0 users,  load average: 0.00, 0.00, 0.00

--- libco/co.c
+++ libco/co.c
@@ -33,22 +33,20 @@ static inline void set_stack_pointer(void *sp)
 #endif
 }
 
-// uintptr_t prev_sp = 0;
-
 static __attribute__ ((noinline)) void stack_switch_call(void *sp, void *entry, uintptr_t arg)
 {
-    uintptr_t prev_sp = get_stack_pointer();
+    // rbp is saved in the sp, but sp is switched to the new stack
     asm volatile(
 #if __x86_64__
-        "movq %0, %%rsp; movq %2, %%rdi; call *%1"
+        "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
         : : "b"((uintptr_t)sp), "d"(entry), "a"(arg) : "memory"
 #else
         "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
         : : "b"((uintptr_t)sp - 8), "d"(entry), "a"(arg) : "memory"
 #endif
     );
-    set_stack_pointer((void *)prev_sp);
-    assert(1);
+    // revert to the previous stack
+
 }
 
 enum co_status {
