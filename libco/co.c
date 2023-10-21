183c142529ff34b9bf9168cd8ec856a14b6c995f
tracer-nju <tracer@nju.edu.cn>
2023-10-20 21:45:25 -0400

    Linux localhost.localdomain 5.1.0-splitfs-default-config #6 SMP Fri Aug 4 06:11:57 EDT 2023 x86_64 x86_64 x86_64 GNU/Linux  21:45:25 up 2 days, 21:53,  0 users,  load average: 0.14, 0.10, 0.06

--- libco/co.c
+++ libco/co.c
@@ -170,10 +170,10 @@ void co_yield (void)
             assert((stack_top & 0xF) == 0);
             // printf("%s start stack top: %p\n", current->name, (void *)stack_top);
 
-            printf("%s stack top: %p\n", current->name, (void *)get_stack_pointer());
+            // printf("%s stack top: %p\n", current->name, (void *)get_stack_pointer());
             stack_switch_call((void *)stack_top, current->func, (uintptr_t)current->arg);
             assert(current != NULL);
-            printf("after stack top: %p\n", (void *)get_stack_pointer());
+            // printf("after stack top: %p\n", (void *)get_stack_pointer());
 
 // #if __x86_64__
 //             printf("%s stack top: %p, start stack top: %p\n", current->name, (void *)get_stack_pointer(), (void *)stack_top);
