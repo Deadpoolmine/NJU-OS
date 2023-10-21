dcd895c303c4f37383977687ba8eb1836e080129
tracer-nju <tracer@nju.edu.cn>
2023-10-20 23:42:44 -0400

    Linux localhost.localdomain 5.1.0-splitfs-default-config #6 SMP Fri Aug 4 06:11:57 EDT 2023 x86_64 x86_64 x86_64 GNU/Linux  23:42:44 up 2 days, 23:50,  0 users,  load average: 0.00, 0.06, 0.02

--- libco/co.c
+++ libco/co.c
@@ -115,7 +115,7 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg)
 
     manage_co(co);
 
-    printf("co '%s' initialized, scheduling\n", co->name == NULL ? "main" : co->name);
+    printf("co '%s' initialized with arg '%s', scheduling\n", co->name == NULL ? "main" : co->name, co->arg);
 
     if (func != NULL)
         co_yield ();
